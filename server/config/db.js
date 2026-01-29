1d68d48b82ede2b86f252dbaaed73d9ebbd21f4f
Antony Judu <168561047+antonyjudu@users.noreply.github.com>
2026-01-29 12:25:04 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,8 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000 // 5 seconds timeout
-    });
-    console.log('Database Connected');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
-    process.exit(1); // exit app if DB fails
-  }
-};
-
-export default connectDB;
+    mongoose.connection.on('connected', () => {
+        console.log('Database Connected');
+    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+}
